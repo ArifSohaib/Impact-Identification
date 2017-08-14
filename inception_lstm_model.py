9fa57515580db8ac32a1afd1a93095caa10fa7b4
Sohaib <arif_sohaib@outlook.com>
2017-08-14 13:48:11 -0400

    changed to bigger lstm layer

--- inception_lstm_model.py
+++ inception_lstm_model.py
@@ -8,16 +8,21 @@ def lstm():
     our CNN to this model predomenently."""
     # Model.
     model = Sequential()
-    model.add(LSTM(2048, return_sequences=True, input_shape=(1,2048)))
+    model.add(LSTM(4096, return_sequences=True, input_shape=(1,2048)))
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(512, activation='elu'))
     model.add(Dropout(0.5))
     model.add(Dense(2, activation='softmax'))
 
     return model
 
-lstm_model = lstm()
-lstm_model.compile(optimizer='adam', loss='categorical_crossentropy')
-lstm_model.fit_generator(generator=data_generator.generate_embeddings(), steps_per_epoch=1500)
+def main():
+    lstm_model = lstm()
+    lstm_model.compile(optimizer='adam', loss='categorical_crossentropy')
+    gen = data_generator.EmbeddingGenerator(embedding_file='period1',frame_file='frames_vid1')
+    lstm_model.fit_generator(generator=gen.generate_embeddings_skip(batch_size=50, skip_prob=0.9), steps_per_epoch=3000)
+    lstm_model.save_weights('lstm_model_weights.h5')
 
-lstm_model.save_weights('lstm_model_weights.h5')
+if __name__ == '__main__':
+    main()
+
